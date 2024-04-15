4d6bb8fe2492b191d5331177c61b59dbf29f0af3
Santo-sk <santhoshkanna98@gmail.com>
2024-04-16 00:01:33 +0530

    login page setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# angular_webApp_UI
-demo project
--- demo_web/src/app/app.module.ts
+++ demo_web/src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from
+    "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './user_login/login-page/login-page.component';
+import { CardModule } from 'primeng/card';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { ButtonModule } from 'primeng/button';
+import { SplitterModule } from 'primeng/splitter';
+import { InputTextModule } from 'primeng/inputtext';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { FormsModule } from "@angular/forms";
+import { AutoCompleteModule } from "primeng/autocomplete";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CardModule,
+    FloatLabelModule,
+    ButtonModule,
+    SplitterModule,
+    InputTextModule,
+    IconFieldModule,
+    InputIconModule,
+    FormsModule,
+    AutoCompleteModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
